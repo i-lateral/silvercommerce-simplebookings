ddab41370b9fefe7a5ad8961213a813f0f78c09f
Morven Lewis-Everley <morven@ilateral.co.uk>
2020-08-17 11:57:47 +0100

    Add tests for events

---
+++ tests/EventDateTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace ilateral\SimpleBookings\Tests;
+
+use ilateral\SimpleBookings\Model\EventDate;
+use ilateral\SimpleBookings\Products\EventProduct;
+use SilverStripe\Dev\SapphireTest;
+use SilverStripe\ORM\FieldType\DBDatetime;
+
+class EventDateTest extends SapphireTest
+{
+    /** 
+     * Defines the fixture file to use for this test class
+     *
+     */
+    protected static $fixture_file = 'Events.yml';
+
+    public function testIsPast()
+    {
+        /** @var EventDate */
+        $one = $this->objFromFixture(EventDate::class, 'workshop_one_one');
+
+        DBDatetime::set_mock_now('2020-06-16 10:00:00');
+        $this->assertEquals(true, $one->isPast());
+
+        DBDatetime::set_mock_now('2020-06-18 10:00:00');
+        $this->assertEquals(true, $one->isPast());
+
+        DBDatetime::set_mock_now('2020-06-14 10:00:00');
+        $this->assertEquals(false, $one->isPast());
+    }
+
+    public function testGetBookedSpaces()
+    {
+        $date = $this->objFromFixture(EventDate::class, 'workshop_one_one');
+        $this->assertEquals(9, $date->getBookedSpaces());
+
+        $date = $this->objFromFixture(EventDate::class, 'workshop_one_two');
+        $this->assertEquals(6, $date->getBookedSpaces());
+
+        $date = $this->objFromFixture(EventDate::class, 'workshop_one_three');
+        $this->assertEquals(6, $date->getBookedSpaces());
+
+        $date = $this->objFromFixture(EventDate::class, 'workshop_two_one');
+        $this->assertEquals(0, $date->getBookedSpaces());
+
+        $date = $this->objFromFixture(EventDate::class, 'workshop_two_two');
+        $this->assertEquals(0, $date->getBookedSpaces());
+
+        $date = $this->objFromFixture(EventDate::class, 'workshop_two_three');
+        $this->assertEquals(4, $date->getBookedSpaces());
+    }
+}
---
+++ tests/EventProductTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace ilateral\SimpleBookings\Tests;
+
+use ilateral\SimpleBookings\Products\EventProduct;
+use SilverStripe\Dev\SapphireTest;
+use SilverStripe\ORM\FieldType\DBDatetime;
+
+/**
+ * Unit tests for the simple bookings functionality
+ *
+ * @author Mo <morven@ilateral.co.uk>
+ * @package simplebookings
+ * @subpackage tests
+ */
+class EventProductTest extends SapphireTest
+{
+
+    /** 
+     * Defines the fixture file to use for this test class
+     *
+     */
+    protected static $fixture_file = 'Events.yml';
+
+    /**
+     * Test if the booked spaces algorythm returns the
+     * correct results when we have a booking that starts
+     * before but ends inside an existing booking
+     * 
+     * @return void
+     */
+    public function testGetCurrentDates()
+    {
+        /** @var EventProduct */
+        $workshop_one = $this->objFromFixture(EventProduct::class, 'workshop_one');
+        /** @var EventProduct */
+        $workshop_two = $this->objFromFixture(EventProduct::class, 'workshop_two');
+
+        // Change the default date (auto cleared in tear down)
+        DBDatetime::set_mock_now('2020-06-01 10:00:00');
+
+        $this->assertEquals(2, $workshop_one->getCurrentDates()->count());
+        $this->assertEquals(3, $workshop_two->getCurrentDates()->count());
+
+        // Change the default date (auto cleared in tear down)
+        DBDatetime::set_mock_now('2020-07-01 10:00:00');
+
+        $this->assertEquals(1, $workshop_one->getCurrentDates()->count());
+        $this->assertEquals(2, $workshop_two->getCurrentDates()->count());
+    }
+
+    public function testGetAvailableSpaces()
+    {
+        /** @var EventProduct */
+        $workshop_one = $this->objFromFixture(EventProduct::class, 'workshop_one');
+
+        $this->assertEquals(10, $workshop_one->getAvailableSpaces('2020-06-16 10:00:00', '2020-06-16 17:00:00'));
+        $this->assertEquals(15, $workshop_one->getAvailableSpaces('2020-07-16 10:00:00', '2020-07-16 17:00:00'));
+        $this->assertEquals(20, $workshop_one->getAvailableSpaces('2020-05-16 10:00:00', '2020-05-16 17:00:00'));
+        $this->assertEquals(0, $workshop_one->getAvailableSpaces('2020-04-16 10:00:00', '2020-04-16 17:00:00'));
+        $this->assertEquals(0, $workshop_one->getAvailableSpaces('2020-08-16 10:00:00', '2020-08-16 17:00:00'));
+
+        /** @var EventProduct */
+        $workshop_two = $this->objFromFixture(EventProduct::class, 'workshop_two');
+
+        $this->assertEquals(22, $workshop_two->getAvailableSpaces('2020-06-16 10:00:00', '2020-06-16 17:00:00'));
+        $this->assertEquals(18, $workshop_two->getAvailableSpaces('2020-07-16 10:00:00', '2020-07-16 17:00:00'));
+        $this->assertEquals(12, $workshop_two->getAvailableSpaces('2020-08-16 10:00:00', '2020-08-16 17:00:00'));
+        $this->assertEquals(0, $workshop_two->getAvailableSpaces('2020-04-16 10:00:00', '2020-04-16 17:00:00'));
+        $this->assertEquals(0, $workshop_two->getAvailableSpaces('2020-09-16 10:00:00', '2020-09-16 17:00:00'));
+    }
+}
