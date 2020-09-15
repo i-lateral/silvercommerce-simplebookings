c3b1c326ce16cb84929b41761171ad099573c5df
Makreig <mark@i-lateral.com>
2020-09-15 14:53:52 +0100

    added note to fully-booked events

--- src/forms/DateOptionsField.php
+++ src/forms/DateOptionsField.php
@@ -2,18 +2,34 @@
 
 namespace ilateral\SimpleBookings\Forms;
 
+use SilverStripe\View\ArrayData;
 use SilverStripe\Forms\OptionsetField;
 
 class DateOptionsField extends OptionsetField
 {
     /**
-     * Extra CSS classes for the FormField container.
+     * Build a field option for template rendering
      *
-     * @var array
+     * @param mixed $value Value of the option
+     * @param string $title Title of the option
+     * @param boolean $odd True if this should be striped odd. Otherwise it should be striped even
+     * @return ArrayData Field option
      */
-    protected $extraClasses = [
-        'field',
-        'form-group',
-        'optionset'
-    ];
+    protected function getFieldOption($value, $title, $odd)
+    {
+        if ($this->isDisabledValue($value)) {
+            $title = _t("Bookings.FullyBooked", "{$title} - Fully Booked", ['title' => $title]);
+        }
+
+        return new ArrayData([
+            'ID' => $this->getOptionID($value),
+            'Class' => $this->getOptionClass($value, $odd),
+            'Role' => 'option',
+            'Name' => $this->getOptionName(),
+            'Value' => $value,
+            'Title' => $title,
+            'isChecked' => $this->isSelectedValue($value, $this->Value()),
+            'isDisabled' => $this->isDisabledValue($value)
+        ]);
+    }
 }
